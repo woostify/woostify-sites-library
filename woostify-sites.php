28ac01c6671eebf1064d241e8a3ec886c7d817ec
duongancol <mr.duongancol@gmail.com>
2020-05-19 15:27:41 +0700

    release version 1.2.4

--- woostify-sites.php
+++ woostify-sites.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:  Woostify Sites Library
  * Description:  Import site demos built with Woostify theme
- * Version:      1.2.3
+ * Version:      1.2.4
  * Author:       Woostify
  * Author URI:   https://woostify.com
  * License:      GPLv2 or later
@@ -24,7 +24,7 @@ if ( ! defined( 'WOOSTIFY_SITES_NAME' ) ) {
 }
 
 if ( ! defined( 'WOOSTIFY_SITES_VER' ) ) {
-	define( 'WOOSTIFY_SITES_VER', '1.2.3' );
+	define( 'WOOSTIFY_SITES_VER', '1.2.4' );
 }
 
 if ( ! defined( 'WOOSTIFY_SITES_FILE' ) ) {
